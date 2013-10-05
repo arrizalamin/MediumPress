1ebd5ffdba093d7c33a9b297c629967f53060c47
arrizalamin <arrizalamin@yahoo.com>
2013-10-05 14:26:18 +0700

    Fix meta.php

--- inc/meta.php
+++ inc/meta.php
@@ -1,3 +1,12 @@
 <div class="meta">
-	<?php (is_home())?the_author_posts_link():'' ?> in <?php the_category(', ') ?> - <span class="meta-time"><i class="icon-bookmark"></i> <?php echo floor(wcount() / 200) + 1 . ' min read' ?></span>
+	<?php if(!is_page()){
+		if(is_home()){
+			the_author_posts_link();
+		}
+		echo ' in ';
+		the_category(', ');
+	}else{
+		echo 'WordPress Page';
+	} ?>
+	 - <span class="meta-time"><i class="icon-bookmark"></i> <?php echo floor(wcount() / 200) + 1 . ' min read' ?></span>
 </div>
