320bb1d719043a052fc7389582a7deb51d1b6dcc
arrizalamin <arrizalamin@yahoo.com>
2013-10-05 22:27:50 +0700

    change 404 error page

--- 404.php
+++ 404.php
@@ -1,5 +1,20 @@
-<?php get_header(); ?>
-
-<?php get_sidebar(); ?>
-
-<?php get_footer(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>404 Error - Medium</title>
+	<link rel="stylesheet" href="<?php echo bloginfo('stylesheet_directory').'/css/404.css' ?>">
+	<link href='http://fonts.googleapis.com/css?family=Droid+Sans:700,400' rel='stylesheet' type='text/css'/>
+</head>
+<body style="background-image: url(<?php echo bloginfo('stylesheet_directory').'/images/404.jpg' ?>)">
+	<div class="container">
+		<div class="error-message">
+			<div class="error-message-wrap">
+				<p>Sorry, but nothing exists here.</p>
+				<span class="error-small">Find something <a href="<?php echo bloginfo('url') ?>">interesting to read</a>.</span>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
