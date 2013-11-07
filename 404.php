94dc0fe922e693176011629ef54d16834a24670c
arrizalamin <arrizalamin@yahoo.com>
2013-11-07 09:05:12 +0700

    Show Blog Name it Title Tag

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
 <head>
 	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>404 Error - Medium</title>
+	<title>404 Error - <?php echo bloginfo('name') ?></title>
 	<link rel="stylesheet" href="<?php echo bloginfo('stylesheet_directory').'/css/404.css' ?>">
 	<link href='http://fonts.googleapis.com/css?family=Droid+Sans:700,400' rel='stylesheet' type='text/css'/>
 </head>
