e60d9f218417e88074c42a847270226129317b99
arrizalamin <arrizalamin@yahoo.com>
2013-11-07 09:06:04 +0700

    Remove Option-tree

--- functions.php
+++ functions.php
@@ -79,30 +79,112 @@
 		$content = ob_get_clean();
 		return sizeof(explode(" ", $content));
 	}
-	/**
-	 * Optional: set 'ot_show_pages' filter to false.
-	 * This will hide the settings & documentation pages.
-	 */
-	add_filter( 'ot_show_pages', '__return_false' );
 
-	/**
-	 * Optional: set 'ot_show_new_layout' filter to false.
-	 * This will hide the "New Layout" section on the Theme Options page.
-	 */
-	add_filter( 'ot_show_new_layout', '__return_false' );
+	/* 
+ * Helper function to return the theme option value. If no value has been saved, it returns $default.
+ * Needed because options are saved as serialized strings.
+ *
+ */
 
-	/**
-	 * Required: set 'ot_theme_mode' filter to true.
-	 */
-	add_filter( 'ot_theme_mode', '__return_true' );
+if ( !function_exists( 'of_get_option' ) ) {
+function of_get_option($name, $default = false) {
+	
+	$optionsframework_settings = get_option('optionsframework');
+	
+	// Gets the unique option id
+	$option_name = $optionsframework_settings['id'];
+	
+	if ( get_option($option_name) ) {
+		$options = get_option($option_name);
+	}
+		
+	if ( isset($options[$name]) ) {
+		return $options[$name];
+	} else {
+		return $default;
+	}
+}
+}
 
-	/**
-	 * Required: include OptionTree.
-	 */
-	load_template( trailingslashit( get_template_directory() ) . 'option-tree/ot-loader.php' );
-	/**
-	 * Theme Options
-	 */
-	load_template( trailingslashit( get_template_directory() ) . '/theme-options.php' );
+/* 
+ * This is an example of how to add custom scripts to the options panel.
+ * This one shows/hides the an option when a checkbox is clicked.
+ */
+
+add_action('optionsframework_custom_scripts', 'optionsframework_custom_scripts');
+
+function optionsframework_custom_scripts() { ?>
+
+<script type="text/javascript">
+jQuery(document).ready(function() {
+
+	jQuery('#example_showhidden').click(function() {
+  		jQuery('#section-example_text_hidden').fadeToggle(400);
+	});
+	
+	if (jQuery('#example_showhidden:checked').val() !== undefined) {
+		jQuery('#section-example_text_hidden').show();
+	}
+	
+});
+</script>
+ 
+<?php
+}
+
+/* 
+ * This is an example of how to override a default filter
+ * for 'text' sanitization and use a different one.
+ */
+
+/*
+
+add_action('admin_init','optionscheck_change_santiziation', 100);
+
+function optionscheck_change_santiziation() {
+	remove_filter( 'of_sanitize_text', 'sanitize_text_field' );
+	add_filter( 'of_sanitize_text', 'of_sanitize_text_field' );
+}
+
+function of_sanitize_text_field($input) {
+	global $allowedtags;
+	$output = wp_kses( $input, $allowedtags);
+	return $output;
+}
+
+*/
+
+/* 
+ * This is an example of how to override the default location and name of options.php
+ * In this example it has been renamed options-renamed.php and moved into the folder extensions
+ */
+
+/*
+
+add_filter('options_framework_location','options_framework_location_override');
+
+function options_framework_location_override() {
+	return array('/extensions/options-renamed.php');
+}
+
+*/
+
+/* 
+ * Here is an example for how to change the menu title name and slug
+ */
+ 
+/*
+ 
+function optionscheck_options_menu_params( $menu ) {
+	
+	$menu['page_title'] = __( 'Hello Options', 'textdomain');
+	$menu['menu_title'] = __( 'Hello Options', 'textdomain');
+	$menu['menu_slug'] = 'hello-options';
+	return $menu;
+}
+
+add_filter( 'optionsframework_menu', 'optionscheck_options_menu_params' );
+
+*/
 
 ?>
