c3cd2b13d2805e0201c5fb0ef40507fa7ee4a111
Ibnu Assayyis <assayyis2@gmail.com>
2016-10-19 17:27:22 +0700

    Fix indentation single.php

--- single.php
+++ single.php
@@ -9,31 +9,31 @@
 		?>
 	</div>
 	<div class="group-and-comment">
-	<div class="page-wrap">
-		<div class="group">
-			<div class="bio-and-post-group">		
-				<div <?php post_class('index-box') ?> id="post-<?php the_ID(); ?>">
+		<div class="page-wrap">
+			<div class="group">
+				<div class="bio-and-post-group">		
+					<div <?php post_class('index-box') ?> id="post-<?php the_ID(); ?>">
 
-					<?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
+						<?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
 
-					<div class="post-title"><?php the_title(); ?></div>
+						<div class="post-title"><?php the_title(); ?></div>
 
-					<div class="post-content">
-						<?php the_content(); ?>
+						<div class="post-content">
+							<?php the_content(); ?>
+						</div>
+						<div class="post-meta-data">
+							<?php the_tags('Tags: ', ', ', '<br />'); ?>
+						</div>
+
 					</div>
-					<div class="post-meta-data">
-						<?php the_tags('Tags: ', ', ', '<br />'); ?>
+
+					<div class="bio">
+						<div class="author-picture"><?php echo get_avatar( get_the_author_meta( 'ID' ),73 ); ?></div>
+						<div class="author-name"><?php the_author_posts_link(); ?></div>
+						<div class="author-bio"><?php the_author_meta('description') ?></div>
+						<div class="post-date"><?php echo (get_the_modified_time() !== get_the_time())?"Updated ".get_the_modified_time('F j, Y'):"Posted ".get_the_time('F j, Y') ?></div>
 					</div>
 
-				</div>
+<?php endwhile; endif; ?>    
 
-				<div class="bio">
-					<div class="author-picture"><?php echo get_avatar( get_the_author_meta( 'ID' ),73 ); ?></div>
-					<div class="author-name"><?php the_author_posts_link(); ?></div>
-					<div class="author-bio"><?php the_author_meta('description') ?></div>
-					<div class="post-date"><?php echo (get_the_modified_time() !== get_the_time())?"Updated ".get_the_modified_time('F j, Y'):"Posted ".get_the_time('F j, Y') ?></div>
-				</div>
-
-			<?php endwhile; endif; ?>    
-
-			<?php get_footer(); ?>
+<?php get_footer(); ?>
