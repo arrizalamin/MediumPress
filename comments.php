69c1b9de54b77663b5386689740eaa6d13c7f6cb
sanakdam <san.akdam@gmail.com>
2016-10-22 14:23:28 +0700

    fix comments.php

--- comments.php
+++ comments.php
@@ -3,8 +3,8 @@
 	if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
 		die ('Please do not load this page directly. Thanks!');
 
-	if ( post_password_required() ) { ?>
-		return "This post is password protected. Enter the password to view comments."
+	if ( post_password_required() ) {
+		return "This post is password protected. Enter the password to view comments.";
 	}
 ?>
 
